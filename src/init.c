284a4dae7bad14dc5d2537f44f55fa27ebcede2e
404NotAFriend <brunodrcgomes@gmail.com>
2026-01-21 21:55:52 +0000

    started to take care of project functions make file and structs , will use circular doubled linked lists on this one ,started init.c

--- src/init.c
+++ src/init.c
@@ -6,7 +6,54 @@
 /*   By: bramalho@student.42porto.com <bramalho>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 17:14:35 by bramalho@st       #+#    #+#             */
-/*   Updated: 2026/01/21 17:19:01 by bramalho@st      ###   ########.fr       */
+/*   Updated: 2026/01/21 21:54:23 by bramalho@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/push_swap.h"
+
+t_node	*create_node(int value)
+{
+	t_node	*new_node;
+
+	new_node = malloc(sizeof(t_node));
+	if (!new_node)
+		return (NULL);
+	new_node->value = value;
+	new_node->index = -1;
+	new_node->pos = 0;
+	new_node->target_pos = 0;
+	new_node->cost_a = 0;
+	new_node->cost_b = 0;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	return (new_node);
+}
+
+void	init_stack(t_stack *stack)
+{
+	stack->top = NULL;
+	stack->size = 0;
+}
+
+void	add_bottom(t_stack *stack, t_node *new_node)
+{
+	t_node	*bottom;
+
+	if (!stack->top)
+	{
+		stack->top = new_node;
+		new_node->prev = new_node;
+		new_node->next = new_node;
+	}
+	else
+	{
+		bottom = stack->top->prev;
+		new_node->pos = stack->top + 1;
+		bottom->next = new_node;
+		new_node->prev = bottom;
+		new_node->next = stack->top;
+		stack->top->prev = new_node;
+	}
+	stack->size++;
+}
--- src/utils/errors.c
+++ src/utils/errors.c
@@ -6,7 +6,18 @@
 /*   By: bramalho@student.42porto.com <bramalho>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 17:21:31 by bramalho@st       #+#    #+#             */
-/*   Updated: 2026/01/21 17:21:48 by bramalho@st      ###   ########.fr       */
+/*   Updated: 2026/01/21 17:58:29 by bramalho@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../includes/push_swap.h"
+
+void	error_exit(t_stack *stack_a, t_stack *stack_b)
+{
+	if (stack_a)
+		free_stack(stack_a);
+	if (stack_b)
+		free_stack(stack_b);
+	write(2, "Error\n", 6);
+	exit(1);
+}
